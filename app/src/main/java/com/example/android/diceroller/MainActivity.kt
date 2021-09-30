2659464464911396c0acf6291066bf7bc07f58fe
ivansteuerberg <irodriguezsteuerberg@danielcastelao.org>
2021-09-30 12:50:25 +0200

    commit solucion

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -24,5 +24,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Esta es mi rama donde ponga mi código
     }
 }
