d040e174b40732b55298456d1f4eefe3ffee70ed
Patricia <pmaceirasvarela@danielcastelao.org>
2021-10-04 10:58:16 +0200

    Resolución ejercicio 3

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -18,25 +18,43 @@ package com.example.android.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // TODO (02) Use findViewById to get a reference to the button
         // and assign it to an immutable variable called rollButton
         val rollButton : Button = findViewById(R.id.roll_button)
-        // TODO (03) set the OnClickListener for the button
-
         rollButton.setOnClickListener{
 
-            // TODO (04) Show a Toast when the OnClickListener is called
+            //  Remove the Toast and instead create a method called rollDice here. You can
 
-            Toast.makeText(this,"Botón clicado",Toast.LENGTH_SHORT).show()
+            rollDice()
         }
         // Esta es mi rama donde ponga mi código
+    }
+
+
+
+    // text of the TextView
+    private fun rollDice(){
+        //  In rollDice get a Random int between 1 and 6
+
+        // Se mete en la variable numeroAleatorio un numero aleatorio del 1 al 6
+        val numeroAleatorio =Random().nextInt(6)+1
+        //  In rollDice use findViewById to get a reference to the TextView
+        // and assign it to an inmutable variable called resultText
+        // Se accede al TextView del layout y al id del TextView
+        val resultado : TextView = findViewById(R.id.result_text)
+        //In rollDice set the random value that you got above as the
+        // Se genera un número aleatorio y se pone en el TextView para que cuando le des el boton aparezca un numero aleatorio
+        resultado.text = numeroAleatorio.toString()
+
+
     }
 }
